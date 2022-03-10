26922db8fde1c1df2ab18c94d7c14dd4b5eec999
dqvist12 <68880900+dqvist12@users.noreply.github.com>
2022-03-10 20:09:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,102 @@
 
 
 
+LPCSTR GetClipBoardService() {
+
+	SC_HANDLE manager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+
+	if (manager == INVALID_HANDLE_VALUE) {
+		std::cout << "Invalid Handle Value." << std::endl;
+	}
+
+	PROCESSESMAP* processesMap = new PROCESSESMAP;
+
+	GetProcessNameAndId(processesMap);
+
+	DWORD bytesNeeded;
+	DWORD servicesNum;
+
+	BOOL status = EnumServicesStatusEx(
+		manager,
+		SC_ENUM_PROCESS_INFO,
+		SERVICE_WIN32,
+		SERVICE_STATE_ALL,
+		NULL,
+		0,
+		&bytesNeeded,
+		&servicesNum,
+		NULL,
+		NULL
+	);
+
+	PBYTE lpBytes = (PBYTE)malloc(bytesNeeded);
+
+	status = EnumServicesStatusEx(
+		manager,
+		SC_ENUM_PROCESS_INFO,
+		SERVICE_WIN32,
+		SERVICE_STATE_ALL,
+		lpBytes,
+		bytesNeeded,
+		&bytesNeeded,
+		&servicesNum,
+		NULL,
+		NULL
+	);
+
+	ENUM_SERVICE_STATUS_PROCESS* lpServiceStatus = (ENUM_SERVICE_STATUS_PROCESS*)lpBytes;
+
+	for (DWORD i = 0; i < servicesNum; i++) {
+
+		const char* cstr = lpServiceStatus[i].lpServiceName;
+
+		//WpnUserService_38a78
+		std::string s = cstr;
+
+
+		string AllServices = cstr;
+		string serviceNameToFind = "cbdhsvc_";
+
+		if (strstr(AllServices.c_str(), serviceNameToFind.c_str()))
+		{
+			return cstr;
+
+			std::cout << cstr << std::endl;
+		}
+	}
+
+	delete processesMap;
+	free(lpBytes);
+
+}
 
 void ClearClipBoard() {
 
-    if (OpenClipboard(NULL)) {
-        // Will clear the current text
+	// Will clear the clipboard history
 
-        EmptyClipboard();
-        CloseClipboard();
-    }
+	const auto hScm = OpenSCManager(nullptr, nullptr, NULL);
+	const auto hSc = OpenService(hScm, GetClipBoardService(), SERVICE_QUERY_STATUS);
+										// ^ The clipboard service
 
-    // Will clear the clipboard history
+	SERVICE_STATUS_PROCESS ssp = {};
 
-    const auto hScm = OpenSCManager(nullptr, nullptr, NULL);
-    const auto hSc = OpenService(hScm, "cbdhsvc_625cd", SERVICE_QUERY_STATUS);
-                                       // ^ The clipboard service
+	DWORD bytesNeeded = 0;
+	QueryServiceStatusEx(hSc, SC_STATUS_PROCESS_INFO, reinterpret_cast <LPBYTE> (&ssp), sizeof(ssp), &bytesNeeded);
 
-    SERVICE_STATUS_PROCESS ssp = {};
+	CloseServiceHandle(hSc);
+	CloseServiceHandle(hScm);
 
-    DWORD bytesNeeded = 0;
-    QueryServiceStatusEx(hSc, SC_STATUS_PROCESS_INFO, reinterpret_cast <LPBYTE> (&ssp), sizeof(ssp), &bytesNeeded);
+	HANDLE ProcPidHANDLE;
+	ProcPidHANDLE = OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, TRUE, ssp.dwProcessId);
+	TerminateProcess(ProcPidHANDLE, 0);
 
-    CloseServiceHandle(hSc);
-    CloseServiceHandle(hScm);
+	if (OpenClipboard(NULL)) {
+		// Will clear the current text
 
-    HANDLE ProcPidHANDLE;
-    ProcPidHANDLE = OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, TRUE, ssp.dwProcessId);
-    TerminateProcess(ProcPidHANDLE, 0);
+		EmptyClipboard();
+		CloseClipboard();
+	}
 }
-
 
 
     //use ClearClipBoard();
